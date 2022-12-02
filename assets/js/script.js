d53b70d4a2633fd9c72d28862d3c81a36a20609a
Aakaash0 <aakashchaurasiya07@gmail.com>
2022-12-02 12:16:09 +0530

    changes in code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,13 @@
 /* Author: 
 
 */
-
+// Hamburger menu
+var hamburger = document.querySelector('.hamburger')
+var nav = document.querySelector('nav')
+hamburger.addEventListener('click', function () {
+  nav.classList.toggle('show')
+  hamburger.classList.toggle('show')
+})
 
 // Filtered tab functionality
 
